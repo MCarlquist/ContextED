1ef06f4fd396b39b9f41e226e92dc5e919154b3e
Max Carlquist <maxcarlquist@icloud.com>
2025-09-17 01:08:40 +0200

    feat: started on home

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  standalone: true,
+  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, SearchbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
